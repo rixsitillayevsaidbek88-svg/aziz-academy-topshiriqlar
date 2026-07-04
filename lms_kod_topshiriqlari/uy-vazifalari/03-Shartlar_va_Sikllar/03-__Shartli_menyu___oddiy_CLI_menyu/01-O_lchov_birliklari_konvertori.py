e981f6d339c290957557faeb122516c72e693516
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-04 16:14:36 +0500

    Add: O'lchov birliklari konvertori [rixsitillayevsaidbek88-svg]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-O_lchov_birliklari_konvertori.py
@@ -0,0 +1,11 @@
+tur = int(input())
+qiymay = int(input())
+if tur == 1:
+    print(qiymay * 1000)
+elif tur == 2:
+    print(qiymay * 60)
+elif tur == 3:
+    print(qiymay * 1000)
+    
+else:
+    print("Notogri tanlov")
