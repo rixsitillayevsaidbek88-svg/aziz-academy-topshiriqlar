40a247ff8759c0eaca1f80d0cf91dfcb21ea4bef
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-04 23:00:06 +0500

    Add AI task: O'rtacha og'irligi

---
+++ ai_topshiriqlar/Ortacha_ogirligi.py
@@ -0,0 +1,15 @@
+# O'rtacha og'irligi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+numbers = [a, b, c, d, e]
+filtered_sum = sum(numbers) - max(numbers) - min(numbers)
+result = filtered_sum // 3
+print(result)
