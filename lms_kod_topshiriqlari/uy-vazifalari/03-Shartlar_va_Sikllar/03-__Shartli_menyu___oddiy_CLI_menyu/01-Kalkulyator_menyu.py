b494d99d22578fb41bfae54fef7abad08096c00f
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-04 16:12:25 +0500

    Add: Kalkulyator menyu [rixsitillayevsaidbek88-svg]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Kalkulyator_menyu.py
@@ -0,0 +1,11 @@
+amal = int(input())
+a = int(input())
+b = int(input())
+if amal == 1:
+    print(a + b)
+elif amal == 2:
+    print(a - b)
+elif amal == 3:
+    print(a * b)
+elif amal == 4:
+    print(a // b)
