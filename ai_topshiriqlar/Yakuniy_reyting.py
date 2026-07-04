9a37af517c3a74cd96e066a8ecb31dc10d4db49b
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-04 21:51:01 +0500

    Add AI task: Yakuniy reyting

---
+++ ai_topshiriqlar/Yakuniy_reyting.py
@@ -0,0 +1,37 @@
+# Yakuniy reyting
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+ishtirokchilar = []
+jami_ball = 0
+for i in range(n):
+    ism, ball_str = input().split()
+    ball = int(ball_str)
+    
+    ishtirokchilar.append((ism, ball))
+    jami_ball += ball
+
+ortacha_butun = jami_ball // n
+
+
+golib_ismi = ""
+maks_ball = -1
+
+for ism, ball in ishtirokchilar:
+    if ball > maks_ball:
+        maks_ball = ball
+        golib_ismi = ism
+    elif ball == maks_ball:
+        if ism < golib_ismi:
+            golib_ismi = ism
+yuqori_sanoq = 0
+for ism, ball in ishtirokchilar:
+    if ball > ortacha_butun:
+        yuqori_sanoq += 1
+
+print(golib_ismi)
+print(ortacha_butun)
+print(yuqori_sanoq)
