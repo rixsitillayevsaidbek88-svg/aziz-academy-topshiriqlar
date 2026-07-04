af7aef650668523edfe73b2402a8d0197a95a014
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-04 12:52:59 +0500

    Add: Mahsulot cheki [rixsitillayevsaidbek88-svg]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,8 @@
+nomi = input().title()
+narx = int(input())
+soni = int(input())
+jami = narx * soni
+print(f"Mahsulot: {nomi}")
+print(f"Narxi: {narx} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami} som")
