6ee6f658d4e75b8b11fc9b9fad43786b6aeebe79
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-05 20:31:31 +0500

    Add AI task: Bank: chegirma hisobi

---
+++ ai_topshiriqlar/Bank_chegirma_hisobi.py
@@ -0,0 +1,15 @@
+# Bank: chegirma hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+u = a + b
+ch = = (u * 70) // 100
+if ch < 100000:
+    y = ch + 10000
+else:
+    y = ch
+print(y)
