2c8ae8276a2cbdc1a11845d342f3f6f0ccc191ba
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-05 10:05:30 +0500

    Add AI task: Maydon hisobi

---
+++ ai_topshiriqlar/Maydon_hisobi.py
@@ -0,0 +1,15 @@
+# Maydon hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+yuza = (a * b) - (c * d)
+perimetr = 2 * (a + b)
+print(yuza)
+print(perimetr)
