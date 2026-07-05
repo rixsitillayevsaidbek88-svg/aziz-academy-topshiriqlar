9e8f0cbc148dd56a01a849754c3bc58d849f8371
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-05 20:35:59 +0500

    Add AI task: Ob-havo stansiyasi: kunlik hisobot

---
+++ ai_topshiriqlar/Ob-havo_stansiyasi_kunlik_hisobot.py
@@ -0,0 +1,30 @@
+# Ob-havo stansiyasi: kunlik hisobot
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+
+u = 0
+j = 0
+
+m -1
+e = ""
+for _ in range(n):
+    q = input().split()
+    nom = q[0]
+    narx = int(q[1])
+    soni = int(q[2])
+    
+    jo = narx * soni
+    
+    u += jo
+    j += soni
+    
+    if jo > m:
+        m = jo
+        e = nom
+print(u)
+print(e)
+print(j)
