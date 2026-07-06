a5b5099d0c6803110bcc1e6435d4f10f5ecee5fb
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-06 09:27:45 +0500

    Add AI task: Davomiylikni hisoblash

---
+++ ai_topshiriqlar/Davomiylikni_hisoblash.py
@@ -0,0 +1,17 @@
+# Davomiylikni hisoblash
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+bosh = a * 60 + b
+tugash = c * 60 + d
+farq = tugash - bosh
+soat = farq // 60
+daqiqa = farq % 60
+print(soat)
+print(daqiqa)
