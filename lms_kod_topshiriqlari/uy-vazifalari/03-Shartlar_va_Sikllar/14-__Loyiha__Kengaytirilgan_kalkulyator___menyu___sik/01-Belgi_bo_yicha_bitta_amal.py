f237fce382e4f6d2f39bfea227ee178b2681c9e0
Saidbek.uz <rixsitillayevsaidbek88@gmail.com>
2026-07-06 17:58:29 +0500

    Add: Belgi bo'yicha bitta amal [rixsitillayevsaidbek88-svg]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,11 @@
+a = int(input())
+amal = input()
+b = int(input())
+if amal == '+':
+    print(a + b)
+elif amal == '-':
+    print(a - b)
+elif amal == '*':
+    print(a * b)
+else:
+    print("Noma'lum amal")
